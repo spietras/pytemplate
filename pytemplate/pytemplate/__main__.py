9e74a9c1d4d4910018ff4cd7d9f0caefc1911942
Sebastian Pietras <seba00767@gmail.com>
2021-04-30 03:55:42 +0200

    Updated CLI management

--- pytemplate/pytemplate/__main__.py
+++ pytemplate/pytemplate/__main__.py
@@ -4,25 +4,22 @@ This module provides basic CLI entrypoint.
 
 """
 
-import argparse
 import sys
 from typing import Optional
 
+import typer
 from pytemplate.subpackage.module import get_something
 
-
-def get_arg_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(description="Simple app")
-    return parser
+cli = typer.Typer()
 
 
-def main() -> Optional[int]:
-    """Package entry point as console script."""
+@cli.command()
+def main(x: int = typer.Option(default=1, help="Dummy argument.")) -> Optional[int]:
+    """Command line interface for pytemplate."""
 
-    args = get_arg_parser().parse_args()
-    print(get_something())
+    typer.echo(get_something(x))  # typer.echo instead of print, because it's better
 
 
 if __name__ == '__main__':
     # entry point for "python -m"
-    sys.exit(main())
+    sys.exit(cli())
